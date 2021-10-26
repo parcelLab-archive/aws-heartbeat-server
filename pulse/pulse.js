9e8f5d51a970fefefd0ae8b6d793dca2a148359e
Svetlomir Bankov <svetlomirbankov@Svetlomirs-MBP.fritz.box>
2021-10-26 13:08:10 +0200

    fix formatting

--- pulse/pulse.js
+++ pulse/pulse.js
@@ -1,5 +1,5 @@
-var AWS = require('aws-sdk')
-var dynamodb = new AWS.DynamoDB()
+const AWS = require('aws-sdk')
+const dynamodb = new AWS.DynamoDB()
 const crypto = require('crypto')
 
 const { GraphQLClient, gql } = require('graphql-request')
@@ -12,14 +12,14 @@ const graphQLClient = new GraphQLClient(graphQlUri, {
 
 exports.handler = function (event, context, callback) {
   const hash = crypto.createHash('sha256')
-  var id
-  var host
-  var category
-  var type
-  var name
-  var timestamp
-  var threshold = '24' // default value - 24 hours
-  var saturday = '0' // default value - exclude saturday
+  let id
+  let host
+  let category
+  let type
+  let name
+  let timestamp
+  let threshold = '24' // default value - 24 hours
+  let saturday = '0' // default value - exclude saturday
 
   if (event.queryStringParameters !== null && event.queryStringParameters !== undefined) {
     if (event.queryStringParameters.host &&
@@ -34,38 +34,38 @@ exports.handler = function (event, context, callback) {
       timestamp = new Date().toISOString()
       hash.update(host + category + type + name)
       id = hash.digest('hex')
-      var params = {
+      const params = {
         Key: {
-          "_id": { 
+          '_id': {
             S: id
-          },
-        },
-        UpdateExpression: "set host= :h,category= :c,typeF= :t,nameF= :n,timestampF = :ti, thresholdF= if_not_exists(thresholdF,:th), saturday= if_not_exists(saturday,:s)",
-        ExpressionAttributeValues: {
-          ":h": {
-            "S": host
-          },
-          ":c": {
-            "S": category
-          },
-          ":t": {
-            "S": type
-          },
-          ":n": {
-            "S": name
-          },
-          ":ti": {
-            "S": timestamp
-          },
-          ":th": {
-            "S": threshold
-          },
-          ":s": {
-            "S": saturday
           }
         },
-        ReturnValues: "ALL_NEW",
-        TableName: "Heartbeat"
+        UpdateExpression: 'set host= :h,category= :c,typeF= :t,nameF= :n,timestampF = :ti, thresholdF= if_not_exists(thresholdF,:th), saturday= if_not_exists(saturday,:s)',
+        ExpressionAttributeValues: {
+          ':h': {
+            'S': host
+          },
+          ':c': {
+            'S': category
+          },
+          ':t': {
+            'S': type
+          },
+          ':n': {
+            'S': name
+          },
+          ':ti': {
+            'S': timestamp
+          },
+          ':th': {
+            'S': threshold
+          },
+          ':s': {
+            'S': saturday
+          }
+        },
+        ReturnValues: 'ALL_NEW',
+        TableName: 'Heartbeat'
       }
       dynamodb.updateItem(params, function (err, data) {
         if (err) {
