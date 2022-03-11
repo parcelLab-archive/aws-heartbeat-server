fe95f3bbb416d658ad1f731121f0bc7c18824bc5
Svetlomir Bankov <svetlomirbankovv@gmail.com>
2022-03-11 15:00:03 +0100

    Remove server functionality and DynamoDB

--- pulse/pulse.js
+++ pulse/pulse.js
@@ -1,5 +1,3 @@
-const AWS = require('aws-sdk')
-const dynamodb = new AWS.DynamoDB()
 const crypto = require('crypto')
 const Cache = require('cache')
 
@@ -14,15 +12,12 @@ const graphQLClient = new GraphQLClient(graphQlUri, {
 const cache = new Cache(15 * 60 * 1000)
 
 exports.handler = function (event, context, callback) {
-  const hash = crypto.createHash('sha256')
-  let id
   let host
   let category
   let type
   let name
   let timestamp
   let threshold = '25' // default value - 25 hours
-  let saturday = '0' // default value - exclude saturday
 
   if (event.queryStringParameters !== null && event.queryStringParameters !== undefined) {
     if (event.queryStringParameters.host &&
@@ -35,41 +30,6 @@ exports.handler = function (event, context, callback) {
       name = event.queryStringParameters.name
       if (event.queryStringParameters.threshold) threshold = event.queryStringParameters.threshold
       timestamp = new Date().toISOString()
-      hash.update(host + category + type + name)
-      id = hash.digest('hex')
-      const params = {
-        Key: {
-          '_id': {
-            S: id
-          }
-        },
-        UpdateExpression: 'set host= :h,category= :c,typeF= :t,nameF= :n,timestampF = :ti, thresholdF= if_not_exists(thresholdF,:th), saturday= if_not_exists(saturday,:s)',
-        ExpressionAttributeValues: {
-          ':h': {
-            'S': host
-          },
-          ':c': {
-            'S': category
-          },
-          ':t': {
-            'S': type
-          },
-          ':n': {
-            'S': name
-          },
-          ':ti': {
-            'S': timestamp
-          },
-          ':th': {
-            'S': threshold
-          },
-          ':s': {
-            'S': saturday
-          }
-        },
-        ReturnValues: 'ALL_NEW',
-        TableName: 'Heartbeat'
-      }
       let thresholdHrs = 25
       try {
         thresholdHrs = parseInt(threshold)
@@ -118,20 +78,6 @@ exports.handler = function (event, context, callback) {
           console.log(`failed to send to zelda for ${type}-${name}-${category}-${host}`)
         }
       }
-      dynamodb.updateItem(params, function (err, data) {
-        if (err) {
-          console.log(err, err.stack)
-          callback({
-            statusCode: err.statusCode,
-            body: 'Error: ' + err
-          })
-        } else {
-          callback(null, {
-            statusCode: 200,
-            body: 'Successful Heartbeat Pulse'
-          })
-        }
-      })
     }
   }
 }
