2f0327cf405eef0a289f1fecb21fe1eb8c65ac04
Svetlomir Bankov <svetlomirbankovv@gmail.com>
2022-03-24 17:33:20 +0100

    simplify lambda and use new gql interface

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Cache = require('cache')
 
 const { GraphQLClient, gql } = require('graphql-request')
-const graphQlUri = 'https://zelda.parcellab.com/graphql'
+const graphQlUri = 'https://zelda.parcellab.com/graphql-next'
 const graphQLClient = new GraphQLClient(graphQlUri, {
   headers: {
     Authorization: 'Token ' + process.env.ZELDA_AUTH_TOKEN
@@ -15,7 +15,6 @@ exports.handler = function (event, context, callback) {
   let category
   let type
   let name
-  let threshold = '25' // default value - 25 hours
 
   if (event.queryStringParameters !== null && event.queryStringParameters !== undefined) {
     if (event.queryStringParameters.host &&
@@ -26,45 +25,29 @@ exports.handler = function (event, context, callback) {
       category = event.queryStringParameters.category
       type = event.queryStringParameters.type
       name = event.queryStringParameters.name
-      if (event.queryStringParameters.threshold) threshold = event.queryStringParameters.threshold
-      let thresholdHrs = 25
-      try {
-        thresholdHrs = parseInt(threshold)
-        if (isNaN(thresholdHrs)) thresholdHrs = 25
-      } catch {
-        console.log(`failed to convert thresholdHrs to int for ${type}-${name}-${category}-${host}`)
-      }
 
       const ts = cache.get(`${type}-${name}-${category}-${host}`)
       if (!(ts && ts > new Date(new Date() - 15 * 60 * 1000))) {
         cache.put(`${type}-${name}-${category}-${host}`, new Date())
         try {
           const query = gql`
-            mutation createHeartbeat ($name: String!,$category: String!,$host: String!,$type: String!,$thresholdHrs: Int!,$lastSuccessAt: DateTime!,$status: String! ){
-            updateCreateHeartbeat(input: {name: $name, category: $category, hostName: $host, type: $type, thresholdHrs: $thresholdHrs, lastSuccessAt: $lastSuccessAt, status: $status}) {
-              id
-              name
-              type
-              category
-              hostName
-              customerId
-              snoozedUntil
-              thresholdHrs
-              errors {
-                field
-                messages
+          mutation createHeartbeat($name: String!, $category: String!, $host: String!, $type: String!){
+            updateCreateHeartbeat(input: { hostName: $host, category: $category, type: $type, name: $name }) {
+          ... on LegacyHeartbeatType {
+                id
+                hostName
+                category
+                type
+                name
               }
             }
-            }
+          }
           `
           const variables = {
             name: name,
             category: category,
             host: host,
-            type: type,
-            lastSuccessAt: new Date(Date.now()),
-            thresholdHrs: thresholdHrs,
-            status: 'closed'
+            type: type
           }
           graphQLClient.request(query, variables)
             .then((data) => console.log('debug', 'pulseLegacyHeartbeatZelda', `Zelda replied with: ${JSON.stringify(data)}`))
